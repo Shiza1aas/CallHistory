01ddc4f7b3161c2cfb8be5fe799b505c65c38f03
Mohammad Shahjahan <ahmsjahan@gmail.com>
2015-07-25 13:34:48 +0530

    Adding home fragment.

--- Home.java
+++ Home.java
@@ -2,7 +2,7 @@ package com.example.shiza.callhistorycontrol;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +23,12 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+
+        return view;
     }
 
 
